b7d74deca97899e1d80172a85e3331cb1768a9f5
AndreiSokol <andrei.sokolov20333@mail.ru>
2022-05-13 08:52:21 +0700

    git-test for commit and push

--- src/main/java/com/tc/cache/CacheApplication.java
+++ src/main/java/com/tc/cache/CacheApplication.java
@@ -3,7 +3,7 @@ package com.tc.cache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /*
- Ming-Top
+ Ming-Top updated 8:51
 */
 @SpringBootApplication
 public class CacheApplication {
